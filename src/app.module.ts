887f66e2fa1d2217bf906af69a7b1ac44975eea3
Fsxerath <frediangarita1999@gmail.com>
2024-06-19 21:22:21 -0500

    add config const

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoryModule } from './category/category.module';
 import { DivisionsModule } from './divisions/divisions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CategoryModule, DivisionsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV,
+      isGlobal: true,
+    }),
+    UsersModule,
+    ProductsModule,
+    CategoryModule,
+    DivisionsModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/config/data.source.ts
@@ -0,0 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+ConfigModule.forRoot({
+  envFilePath: process.env.NODE_ENV,
+});
+
+const configServices = new ConfigService();
+
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: configServices.get('DB_HOST'),
+  port: configServices.get('DB_PORT'),
+  username: configServices.get('DB_USER'),
+  password: configServices.get('DB_PASSWORD'),
+  database: configServices.get('DB_NAME'),
+  synchronize: true,
+  logging: false,
+};
+export const appDS = new DataSource(DataSourceConfig);
